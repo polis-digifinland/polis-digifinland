094ab85f8b22996cf44d4ec54296ec2fbf27ef93
Toni Laitamo <toni.laitamo@tietoevry.com>
2023-05-02 10:57:27 +0300

    Update docker_images-build-and-push.sh

--- scripts/docker_images-build-and-push.sh
+++ scripts/docker_images-build-and-push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# usage: ./path_to_this_script example.com/project/repository tagname VARIABLE="build-time-variables"
+# usage: ./path_to_this_script example.com/project/repository tagname VARIABLE="build-time-variables" VARIABLE="build-time-variables"
 
 # Script takes registry repository location and image tag as arguments.
 # Example:
-# ./scripts/docker_images-build-and-push.sh example.com/project/repository tagname VARIABLE="build-time-variables"
+# ./scripts/docker_images-build-and-push.sh example.com/project/repository tagname VARIABLE="build-time-variables" VARIABLE="build-time-variables"
 
 # before running, first authenticate and configure gcloud:
 # gcloud auth login
@@ -11,7 +11,7 @@
 # gcloud config set project polis-gcp-project-name-example
 
 cd ./polis/
-docker build . -f file-server/Dockerfile -t $1/polis-file-server:$2 --build-arg $3
+docker build . -f file-server/Dockerfile -t $1/polis-file-server:$2 --build-arg $3 --build-arg $4
 
 cd ./math/
 docker build . -t $1/polis-math:$2 --build-arg $3
