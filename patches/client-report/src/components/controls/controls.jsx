7836e345f1813ced904364f682493dc5a410ab4e
laitaton <91537360+laitaton@users.noreply.github.com>
2025-08-15 06:55:57 +0300

    Fix client-report translations

--- patches/client-report/src/components/controls/controls.jsx
+++ patches/client-report/src/components/controls/controls.jsx
@@ -4,7 +4,7 @@
 import Checkbox from "../framework/checkbox.jsx";
 import React from "react";
 import settings from "../../settings";
-import { useTranslation, i18n } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 const Controls = ({
   onAutoRefreshEnabled,
@@ -22,7 +22,7 @@ const Controls = ({
     }
   };
   
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   
   if (i18n && i18n.language) {
     document.documentElement.lang = i18n.language; //This is too slow for google translate
